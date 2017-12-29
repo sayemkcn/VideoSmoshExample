34aa5ae4272b94e75d7eaedb4c88a3b813331088
Sayem Hossain <sayemkcn@gmail.com>
2017-12-29 13:18:06 +0600

    Asked for permission

--- app/src/main/java/xyz/rimon/videomash/MainActivity.java
+++ app/src/main/java/xyz/rimon/videomash/MainActivity.java
@@ -1,8 +1,10 @@
 package xyz.rimon.videomash;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
@@ -19,6 +21,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
+import xyz.rimon.videomash.utils.PermissionUtil;
 import xyz.rimon.videomash.utils.StorageUtil;
 import xyz.rimon.videomash.utils.Toaster;
 
@@ -48,6 +51,12 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+        // Ask for permission
+        PermissionUtil.askForPermission(this, Manifest.permission.CAMERA);
+        PermissionUtil.askForPermission(this, Manifest.permission.RECORD_AUDIO);
+        PermissionUtil.askForPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (!PermissionUtil.hasPermission(this, Manifest.permission.CAMERA) || !PermissionUtil.hasPermission(this, Manifest.permission.RECORD_AUDIO))
+            return;
         recorder = new MediaRecorder();
 //        initRecorder();
         setContentView(R.layout.activity_main);
@@ -174,4 +183,30 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         this.recorder = initRecorder(this.recorder);
         this.recorder = prepareRecorder(this, this.recorder, this.holder);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 2222: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    this.recreate();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    this.finish();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 }
+
